453fcd17147d3aa390756213ab1cfa32aa53bcb4
CristianN01 <cristiannechifor71@gmail.com>
2024-05-13 16:45:08 +0200

    creato variabili per numero randomico da 1 a 6, creato variabili per calcolare quale numero e maggiore e la funzione da svolgere, finale il numero apparira in console sia il nostro che quello del computer con la rispettiva vincita o perdita

---
+++ Gioco dei dadi/JS/script.js
@@ -0,0 +1,10 @@
+const myRandomNb = parseInt(Math.round(Math.random()* 6));
+const randomComputerNb = parseInt(Math.round(Math.random()* 6));
+
+if(myRandomNb > randomComputerNb){
+    console.log('HAI VINTO')
+} else ( randomComputerNb > myRandomNb) 
+    console.log('HAI PERSO, RITENTA')
+
+
+console.log('Il tuo numero :' + myRandomNb + ' ' + 'Il numero del computer:' + randomComputerNb)
