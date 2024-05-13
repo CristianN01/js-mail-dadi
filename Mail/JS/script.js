6b9a9ccc51a7c84998775217a4be37f8571b2a33
CristianN01 <cristiannechifor71@gmail.com>
2024-05-13 17:13:39 +0200

    creato la struttura, aggiunto un array con scritto dentro le email valide per enrtrare, creato un prompt per inserire l'email, aggiunto una variabile per lesito (se l'email e valida o no)

---
+++ Mail/JS/script.js
@@ -0,0 +1,8 @@
+const emailList = ['cristiannechifor71@gmail.com','giggino01@gmai.com']
+const emailText = prompt('type an Email')
+
+if ( emailList.includes(emailText)){
+    alert('your email is valid')
+} else{
+    alert('your email is not valid')
+}
