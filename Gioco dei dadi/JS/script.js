769284586c5c0d67837162732f54605bc8e5497b
CristianN01 <cristiannechifor71@gmail.com>
2024-05-13 16:53:26 +0200

    corretto la sintassi

--- Gioco dei dadi/JS/script.js
+++ Gioco dei dadi/JS/script.js
@@ -3,8 +3,8 @@ const randomComputerNb = parseInt(Math.round(Math.random()* 6));
 
 if(myRandomNb > randomComputerNb){
     console.log('HAI VINTO')
-} else ( randomComputerNb > myRandomNb) 
+} else {
     console.log('HAI PERSO, RITENTA')
-
+}
 
 console.log('Il tuo numero :' + myRandomNb + ' ' + 'Il numero del computer:' + randomComputerNb)
